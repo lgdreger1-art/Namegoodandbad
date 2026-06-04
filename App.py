239bc866cc545229f94f9640be5cf4938b0e0a0d
lgdreger1-art <lgdreger1@gmail.com>
2026-06-04 08:23:50 +0600

    Update and rename newfile.py to App.py

---
+++ App.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template_string
+
+app = Flask(__name__)
+
+html_template = '''
+<form method="POST">
+    Name bol: <input name="L">
+    <input type="submit" value="Check">
+</form>
+{% if result %}
+    <p>{{ result }}</p>
+{% endif %}
+'''
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    result = None
+    if request.method == 'POST':
+        L = request.form.get('L', '').lower().strip()
+        names = ['abdullah', 'abid']
+        if L in names:
+            result = 'Ek nomborer lucca khankir pola'
+        else:
+            result = 'Era to valo sele'
+    return render_template_string(html_template, result=result)
+
+if __name__ == '__main__':
+    app.run()
--- newfile.py
+++
@@ -1,6 +0,0 @@
-L=input('name bol: ')
-name=['abdullah','abid']
-if L in name:
-    print('ek nomborer lucca khankir pola')
-else:
-    print('era to valo sele')
